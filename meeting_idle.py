01abcf32986cdbde05d0126de05b1196b66176f7
Hem-G <hemchander.gunashekar@razorpay.com>
2020-12-30 11:40:32 +0530

    Create meeting_idle.py

---
+++ meeting_idle.py
@@ -0,0 +1,22 @@
+print (1+1)
+print (105+10)
+
+num = [2,6,3,10,223,13521,21,314,0]
+number_ascending = sorted(num, reverse = False)
+number_descending = sorted(num, reverse = True)
+print 'numbers ascending', number_ascending
+print 'numbers descending', number_descending
+
+a=[1, 'hello']
+print 'a=', a
+a_type = type(a).__name__
+print 'a type:', a_type
+
+b=1+1
+c=105+10
+d=b*c
+
+print 'b=', b
+print 'c=', c
+print 'd=b*c'
+print 'd=',d
